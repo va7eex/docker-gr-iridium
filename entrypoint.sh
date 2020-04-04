3ca4672f06285791b9e3a06ec773b865d427c26c
dr <dr@docker-dev.3.14159.space>
2020-04-04 00:00:57 -0700

    removing windows newlines

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,12 @@ cp /usr/share/zoneinfo/$TZ /etc/localtime
 echo $TZ > /etc/timezone
 
 #create config file from environment values
-echo "[osmosdr-source]" > /usr/share/sdr.conf
-echo "samplerate=${SAMPLERATE}" >> /usr/share/sdr.conf
-echo "center_freq=${CENTER_FREQ}" >> /usr/share/sdr.conf
-echo "device_args='${SDR}=${SDR_INDEX},bias=${BIAS}'" >> /usr/share/sdr.conf
-echo "bandwidth=${BANDWIDTH}" >> /usr/share/sdr.conf
-echo "gain=${GAIN}" >> /usr/share/sdr.conf
-echo "if_gain=${IF_GAIN}" >> /usr/share/sdr.conf
+echo "[osmosdr-source]" > /tmp/sdr.conf
+echo "samplerate=${SAMPLERATE}" >> /tmp/sdr.conf
+echo "center_freq=${CENTER_FREQ}" >> /tmp/sdr.conf
+echo "device_args='${SDR}=${SDR_INDEX},bias=${BIAS}'" >> /tmp/sdr.conf
+echo "bandwidth=${BANDWIDTH}" >> /tmp/sdr.conf
+echo "gain=${GAIN}" >> /tmp/sdr.conf
+echo "if_gain=${IF_GAIN}" >> /tmp/sdr.conf
 
-iridium-extractor -D 4 /usr/share/sdr.conf | grep "A:OK" > /opt/output/output.bits
+iridium-extractor -D 4 /tmp/sdr.conf | grep "A:OK" > /opt/output/output.bits
