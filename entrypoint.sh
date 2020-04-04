0a81dda1558ea293a6698025fd96dfcccc758304
va7eex <25559687+va7eex@users.noreply.github.com>
2020-04-03 21:30:09 -0700

    Dynamically creating config files

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,13 @@
 cp /usr/share/zoneinfo/$TZ /etc/localtime
 echo $TZ > /etc/timezone
 
-iridium-extractor -D 4 /usr/src/gr-iridium/examples/$SDR.conf | grep "A:OK" > /opt/output/output.bits
+#create config file from environment values
+echo "[osmosdr-source]" > /usr/share/sdr.conf
+echo "samplerate=${SAMPLERATE}" >> /usr/share/sdr.conf
+echo "center_freq=${CENTER_FREQ}" >> /usr/share/sdr.conf
+echo "device_args='${SDR}=${SDR_INDEX},bias=${BIAS}'" >> /usr/share/sdr.conf
+echo "bandwidth=${BANDWIDTH}" >> /usr/share/sdr.conf
+echo "gain=${GAIN}" >> /usr/share/sdr.conf
+echo "if_gain=${IF_GAIN}" >> /usr/share/sdr.conf
+
+iridium-extractor -D 4 /usr/share/sdr.conf | grep "A:OK" > /opt/output/output.bits
