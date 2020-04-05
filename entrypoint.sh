47b3eb096fa722fdeec461e4959c045d3d885035
dr <dr@docker-dev.3.14159.space>
2020-04-04 17:50:08 -0700

    whitespace issues?

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-
-echo "hello world"
 #where is that \r?
 #cp /usr/share/zoneinfo/$TZ /etc/localtime
 echo $TZ > /etc/timezone
-
+#
 #create config file from environment values
 touch /tmp/sdr.conf
 echo "[osmosdr-source]" > /tmp/sdr.conf
@@ -14,5 +12,5 @@ echo "device_args='${SDR}=${SDR_INDEX},bias=${BIAS}'" >> /tmp/sdr.conf
 echo "bandwidth=${BANDWIDTH}" >> /tmp/sdr.conf
 echo "gain=${GAIN}" >> /tmp/sdr.conf
 echo "if_gain=${IF_GAIN}" >> /tmp/sdr.conf
-
+#
 iridium-extractor -D 4 /tmp/sdr.conf | grep "A:OK" > /opt/output/output.bits
