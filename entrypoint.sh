9239048189b5a0fedf21291d96a2da684b7d2684
va7eex <25559687+va7eex@users.noreply.github.com>
2020-04-04 17:55:07 -0700

    finally got rid of the \r's

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-#where is that \r?
-#cp /usr/share/zoneinfo/$TZ /etc/localtime
+
+cp /usr/share/zoneinfo/$TZ /etc/localtime
 echo $TZ > /etc/timezone
-#
+
 #create config file from environment values
+
 touch /tmp/sdr.conf
 echo "[osmosdr-source]" > /tmp/sdr.conf
 echo "samplerate=${SAMPLERATE}" >> /tmp/sdr.conf
@@ -12,5 +13,6 @@ echo "device_args='${SDR}=${SDR_INDEX},bias=${BIAS}'" >> /tmp/sdr.conf
 echo "bandwidth=${BANDWIDTH}" >> /tmp/sdr.conf
 echo "gain=${GAIN}" >> /tmp/sdr.conf
 echo "if_gain=${IF_GAIN}" >> /tmp/sdr.conf
-#
+
 iridium-extractor -D 4 /tmp/sdr.conf | grep "A:OK" > /opt/output/output.bits
+
